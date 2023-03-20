e61b7c246893e83116ef50d51f2298ded10fc6c9
raja24130 <117657790+raja24130@users.noreply.github.com>
2023-03-20 13:27:08 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 # Load the breast cancer dataset
