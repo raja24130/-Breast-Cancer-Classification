94458d6a015d2d05ce4820a2d2e3d6f5583a6ba0
raja24130 <117657790+raja24130@users.noreply.github.com>
2023-03-20 13:19:19 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-import sklearn.datasets
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
