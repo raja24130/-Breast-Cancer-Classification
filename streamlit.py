21f6b7d51521313c5dbb6a1d780b5da063fd96a0
raja24130 <117657790+raja24130@users.noreply.github.com>
2023-03-20 12:54:27 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -2,51 +2,51 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from sklearn.datasets import load_breast_cancer
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 
-# Load breast cancer dataset
-data = load_breast_cancer()
-X = data.data
-y = data.target
+# Load the breast cancer dataset
+breast_cancer_dataset = load_breast_cancer()
 
-# Split dataset into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+# Create a DataFrame from the dataset
+breast_cancer_dataset = pd.DataFrame(np.c_[data['data'], data['target']], columns=np.append(data['feature_names'], ['target']))
 
-# Train a Random Forest classifier
-classifier = RandomForestClassifier(n_estimators=100, random_state=0)
-classifier.fit(X_train, y_train)
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(df.iloc[:, :-1], df.iloc[:, -1], test_size=0.2, random_state=42)
 
-# Make predictions on the testing set
-y_pred = classifier.predict(X_test)
+# Create a logistic regression classifier
+clf = LogisticRegression(random_state=42)
 
-# Calculate accuracy score
-accuracy = accuracy_score(y_test, y_pred)
+# Train the classifier on the training set
+clf.fit(X_train, y_train)
 
-# Create Streamlit app
-st.title("Breast Cancer Classification")
-st.write("Accuracy:", accuracy)
-
-# Add input fields for user to enter features
-col1, col2 = st.beta_columns(2)
-with col1:
-    mean_radius = st.number_input("Mean radius", value=15.0, min_value=0.0, max_value=50.0)
-    mean_texture = st.number_input("Mean texture", value=20.0, min_value=0.0, max_value=50.0)
-    mean_perimeter = st.number_input("Mean perimeter", value=100.0, min_value=0.0, max_value=300.0)
-
-with col2:
-    mean_area = st.number_input("Mean area", value=500.0, min_value=0.0, max_value=2500.0)
-    mean_smoothness = st.number_input("Mean smoothness", value=0.1, min_value=0.0, max_value=1.0)
-
-# Create a prediction button
-if st.button("Predict"):
-    # Create a numpy array with user input data
-    user_input = np.array([[mean_radius, mean_texture, mean_perimeter, mean_area, mean_smoothness]])
-    # Make prediction using trained classifier
-    prediction = classifier.predict(user_input)
-    # Display prediction result
+# Define a function to take in user inputs and make a prediction
+def predict(model, input_data):
+    prediction = model.predict(input_data)
     if prediction[0] == 0:
-        st.write("Result: Benign")
+        pred = 'Malignant'
     else:
-        st.write("Result: Malignant")
+        pred = 'Benign'
+    return pred
+
+# Create the Streamlit app
+def app():
+    st.title("Breast Cancer Prediction App")
+    
+    # Add a sidebar with user inputs
+    st.sidebar.header("User Input Features")
+    
+    mean_radius = st.sidebar.slider("Mean radius", float(df.mean_radius.min()), float(df.mean_radius.max()), float(df.mean_radius.mean()))
+    mean_texture = st.sidebar.slider("Mean texture", float(df.mean_texture.min()), float(df.mean_texture.max()), float(df.mean_texture.mean()))
+    mean_perimeter = st.sidebar.slider("Mean perimeter", float(df.mean_perimeter.min()), float(df.mean_perimeter.max()), float(df.mean_perimeter.mean()))
+    mean_area = st.sidebar.slider("Mean area", float(df.mean_area.min()), float(df.mean_area.max()), float(df.mean_area.mean()))
+    mean_smoothness = st.sidebar.slider("Mean smoothness", float(df.mean_smoothness.min()), float(df.mean_smooth
+# Allow user to input data
+input_data = {}
+for feature in X.columns:
+    input_data[feature] = st.sidebar.slider(feature, float(X[feature].min()), float(X[feature].max()), float(X[feature].mean()))
+
+# Make prediction and display result
+result = predict(clf, pd.DataFrame(input_data, index=[0]))
+st.write("Prediction:", result)
