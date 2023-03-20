9269af7b19e59389cc1e7f207aa65193937bc8c7
raja24130 <117657790+raja24130@users.noreply.github.com>
2023-03-20 13:52:38 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -3,8 +3,6 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
-# Load the breast cancer dataset
-breast_cancer_dataset = sklearn.datasets.load_breast_cancer()
 X = data_frame.drop(columns='label', axis=1)
 Y = data_frame['label']
 # Train the logistic regression model
