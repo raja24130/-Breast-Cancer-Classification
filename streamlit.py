7ee779c1cae81a6db1ee2332006b6ceb8ca90925
raja24130 <117657790+raja24130@users.noreply.github.com>
2023-03-20 13:53:46 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -5,8 +5,6 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 # Train the logistic regression model
 clf = LogisticRegression()
-clf.fit(X, y)
-
 # Define a function to make predictions
 input_data = (13.54,14.36,87.46,566.3,0.09779,0.08129,0.06664,0.04781,0.1885,0.05766,0.2699,0.7886,2.058,23.56,0.008462,0.0146,0.02387,0.01315,0.0198,0.0023,15.11,19.26,99.7,711.2,0.144,0.1773,0.239,0.1288,0.2977,0.07259)
 
