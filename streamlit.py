f75b89435020b0b6e95960cfa5c25da9f488aebc
raja24130 <117657790+raja24130@users.noreply.github.com>
2023-03-20 13:44:48 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -13,12 +13,22 @@ clf = LogisticRegression()
 clf.fit(X, y)
 
 # Define a function to make predictions
-def predict(data):
-    prediction = clf.predict(data)
-    if prediction[0] == 0:
-        return "Malignant"
-    else:
-        return "Benign"
+input_data = (13.54,14.36,87.46,566.3,0.09779,0.08129,0.06664,0.04781,0.1885,0.05766,0.2699,0.7886,2.058,23.56,0.008462,0.0146,0.02387,0.01315,0.0198,0.0023,15.11,19.26,99.7,711.2,0.144,0.1773,0.239,0.1288,0.2977,0.07259)
+
+# change the input data to a numpy array
+input_data_as_numpy_array = np.asarray(input_data)
+
+# reshape the numpy array as we are predicting for one datapoint
+input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+
+prediction = model.predict(input_data_reshaped)
+print(prediction)
+
+if (prediction[0] == 0):
+  print('The Breast cancer is Malignant')
+
+else:
+  print('The Breast Cancer is Benign')
 
 # Create the Streamlit app
 st.title("Breast Cancer Prediction using Logistic Regression")
